4088ed335f8b1280566a88e4341e3f13e0f897ca
gabighiunghius <56878112+gabighiunghius@users.noreply.github.com>
2020-02-11 18:43:11 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nr = new List<int>();
+            HashSet<int> num = new HashSet<int>();
+
+
+            Stopwatch stopwatch = new Stopwatch();
+            int g = 0;
+            // Begin timing.
+            stopwatch.Start();
+
+            // Do something.
+            for (int i = 0; i < 1000000; i++)
+            {
+                num.Add(i);
+            }
+            for(int i = 0; i < nr.Count; i++)
+            {
+                if (i > 9999 && i < 10001) g = i;
+
+            }
+
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+}
